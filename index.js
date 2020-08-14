d653c4a68234d04758060176cadf2101a083a895
Maria Viji George <mariavijigeorge@gmail.com>
2020-08-14 11:41:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1= Math.floor(Math.random()*6+1);
+var randomNumber2= Math.floor(Math.random()*6+1);
+document.querySelectorAll("img")[0].setAttribute("src","images/dice"+ randomNumber1+".png");
+document.querySelectorAll("img")[1].setAttribute("src","images/dice"+ randomNumber2+".png");
+var winner;
+if(randomNumber1>randomNumber2){
+  winner="🏆 Player 1 Wins!";
+}
+else if(randomNumber1<randomNumber2){
+  winner="🏆 Player 2 Wins!";
+}
+else{
+  winner="🤝🏻 Draw!";
+}
+document.querySelector("h1").innerHTML=winner;
